b74eb9e58c96d05a0989b91f6c3d400afd43d37d
Nicolas Aguilar <nicolasthedoors2014@gmail.com>
2017-12-06 22:47:17 -0300

    mejorando el estilo de codigo de passenger.rb segun This Ruby style guide

--- bin/passenger.rb
+++ bin/passenger.rb
@@ -5,21 +5,21 @@ require './bin/user'
 class Passenger < User
   attr_accessor :miles
 
-    class << self
-      def new_from_hash user
-        Passenger.new(user['name'], user['email'],
-                      user['phone'], user['balance'], user['miles'])
-      end
+  class << self
+    def new_from_hash(user)
+      new(user['name'], user['email'],
+          user['phone'], user['balance'], user['miles'])
     end
+  end
 
-    def initialize(name, email, phone, balance, miles)
-        super(name, email, phone, balance)
-        @miles = miles
-        @type = 'passenger'
-    end
+  def initialize(name, email, phone, balance, miles)
+    super(name, email, phone, balance)
+    @miles = miles
+    @type = 'passenger'
+  end
 
-    def to_hash
-        return { 'name' => @name, 'email' => @email, 'phone' => @phone, 'balance' => @balance,
-                'type' => @type, 'miles' => @miles }
-    end
+  def to_hash
+    { 'name' => @name, 'email' => @email, 'phone' => @phone,
+      'balance' => @balance, 'type' => @type, 'miles' => @miles }
+  end
 end
