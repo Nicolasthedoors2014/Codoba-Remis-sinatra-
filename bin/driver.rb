ba11baaa4bd69b0f3eaaa4b1f50a02fb4b0dc5af
Nicolas Aguilar <nicolasthedoors2014@gmail.com>
2017-12-06 22:35:58 -0300

    mejorando el estilo de codigo de driver.rb segun This Ruby style guide

--- bin/driver.rb
+++ bin/driver.rb
@@ -6,12 +6,11 @@ class Driver < User
   attr_reader :fare, :rating, :licence
 
   class << self
-    def new_from_hash user
-      Driver.new(user['name'], user['email'],user['phone'], user['balance'],
-                user['licence'],user['fare'], user['rating'])
+    def new_from_hash(user)
+      new(user['name'], user['email'], user['phone'], user['balance'],
+          user['licence'], user['fare'], user['rating'])
     end
   end
-
 
   def initialize(name, email, phone, balance, licence, fare, rating)
     super(name, email, phone, balance)
@@ -20,32 +19,30 @@ class Driver < User
     @type = 'driver'
     @rating = rating
     # if rating is nil (new user), initialize rating as [rating_count, rating]
-    @rating ||= [[0,1],[0,2],[0,3],[0,4],[0,5]]
-
+    @rating ||= [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5]]
   end
 
   # Methods.
 
   def update_rating(value)
-    @rating[value -1][0] += 1
+    @rating[value - 1][0] += 1
   end
 
   def avarage_rating
     avarage = 0
     count = 0
-    self.rating.each do |rating_count, rating|
+    rating.each do |rating_count, rating|
       if rating_count != 0
-        avarage += rating_count*rating
+        avarage += rating_count * rating
         count += rating_count
       end
     end
-    count != 0 ? (avarage/count).round(0) : 0
+    count != 0 ? (avarage / count).round(0) : 0
   end
 
   def to_hash
-      return { 'name' => @name, 'email' => @email, 'phone' => @phone,
-              'balance' => @balance, 'type' => @type, 'licence' => @licence,
-              'fare' => @fare, 'rating' => @rating}
+    { 'name' => @name, 'email' => @email, 'phone' => @phone,
+      'balance' => @balance, 'type' => @type, 'licence' => @licence,
+      'fare' => @fare, 'rating' => @rating }
   end
-
 end
