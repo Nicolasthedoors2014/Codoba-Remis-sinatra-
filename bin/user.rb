34c9167c686d3d09f7cb35e1952f1f9808e72a09
Nicolas Aguilar <nicolasthedoors2014@gmail.com>
2017-12-06 22:48:13 -0300

    mejorando el estilo de codigo de user.rb segun This Ruby style guide

--- bin/user.rb
+++ bin/user.rb
@@ -1,18 +1,19 @@
-# Base class for users. Contains attributes and methods that everyone needs.
+
+# Base class for users.
+# Contains attributes and methods that everyone needs.
 
 class User
-    @@id_count = 0
-    attr_accessor :balance
-    attr_reader :id, :name, :email, :phone, :type
+  @@id_count = 0
+  attr_accessor :balance
+  attr_reader :id, :name, :email, :phone, :type
 
-    def initialize(name, email, phone, balance)
-        @name = name
-        @email = email
-        @phone = phone
-        @balance = balance.round(2)
-        @type = 'user'
-        @id = @@id_count
-        @@id_count += 1
-    end
-
+  def initialize(name, email, phone, balance)
+    @name = name
+    @email = email
+    @phone = phone
+    @balance = balance.round(2)
+    @type = 'user'
+    @id = @@id_count
+    @@id_count += 1
+  end
 end
